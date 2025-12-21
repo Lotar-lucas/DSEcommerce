f98834e7dd050680029a99168e0d97766da0cbc0
Lotar <amaral.lucas.lotar#gmail.com>
2025-12-21 02:41:05 -0300

    OrderDTO, get order by id

---
+++ src/main/java/com/example/dscommerce/services/OrderService.java
@@ -0,0 +1,24 @@
+package com.example.dscommerce.services;
+
+import com.example.dscommerce.dto.OrderDTO;
+import com.example.dscommerce.entities.Order;
+import com.example.dscommerce.repositories.OrderRepository;
+import com.example.dscommerce.services.exceptions.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OrderService {
+
+  private final OrderRepository orderRepository;
+
+  public OrderService(OrderRepository orderRepository) {
+    this.orderRepository = orderRepository;
+  }
+
+  @Transactional(readOnly = true)
+  public OrderDTO findById(Long id){
+    Order order = orderRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Order not found"));
+    return new OrderDTO(order);
+  };
+}
