9971cfe4ac969217c822320148ed4bf9f812381d
Lotar <amaral.lucas.lotar#gmail.com>
2025-12-22 18:58:43 -0300

    eliminate undue imports

--- src/main/java/com/example/dscommerce/repositories/OrderRepository.java
+++ src/main/java/com/example/dscommerce/repositories/OrderRepository.java
@@ -1,13 +1,6 @@
 package com.example.dscommerce.repositories;
 
 import com.example.dscommerce.entities.Order;
-import com.example.dscommerce.entities.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-
-}
+public interface OrderRepository extends JpaRepository<Order, Long> {}
